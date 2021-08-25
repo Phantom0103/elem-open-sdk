f1b16f68605b9ed01df995bf75be12beee96410f
周伟 <vszhou_wei2007@126.com>
2021-08-25 16:09:06 +0800

    获取未处理部分退单

---
+++ src/main/java/elem/retail/dto/order/OrderPartRefundUntreListNewResult.java
@@ -0,0 +1,42 @@
+package elem.retail.dto.order;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import com.elem.retail.api.ElemResponseData;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @Author zhouw
+ * @Description
+ * @Date 2021-08-25
+ */
+@Getter
+@Setter
+public class OrderPartRefundUntreListNewResult extends ElemResponseData {
+
+    private static final long serialVersionUID = -5135600517840873672L;
+
+    private int total;
+
+    @JSONField(name = "list")
+    private List<PartRefundTodo> partRefundTodos;
+
+    @Getter
+    @Setter
+    public static class PartRefundTodo {
+        @JSONField(name = "order_id")
+        private String orderId;
+        @JSONField(name = "refund_id")
+        private String refundId;
+        @JSONField(name = "apply_time")
+        private Date applyTime;
+        @JSONField(name = "cancel_reason")
+        private String cancelReason;
+        @JSONField(name = "addition_reason")
+        private String additionReason;
+        private List<String> photos;
+    }
+}
