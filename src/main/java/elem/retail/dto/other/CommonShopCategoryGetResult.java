c12be4006c479a1107722b4e76d117a7e258672f
周伟 <vszhou_wei2007@126.com>
2021-09-01 19:00:00 +0800

    CommonShopCategoryGetResult补充getter setter

--- src/main/java/elem/retail/dto/other/CommonShopCategoryGetResult.java
+++ src/main/java/elem/retail/dto/other/CommonShopCategoryGetResult.java
@@ -2,12 +2,16 @@ package elem.retail.dto.other;
 
 import com.alibaba.fastjson.annotation.JSONField;
 import com.elem.retail.api.ElemResponseData;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * @Author zhouw
  * @Description
  * @Date 2021-09-01
  */
+@Getter
+@Setter
 public class CommonShopCategoryGetResult extends ElemResponseData {
 
     private static final long serialVersionUID = -2761846264373670035L;
