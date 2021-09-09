e74ab5fdd90559e79960c03c3d18aa4bf9fc1120
周伟 <vszhou_wei2007@126.com>
2021-09-09 11:55:45 +0800

    设置商户公告

---
+++ src/main/java/com/elem/retail/api/executor/ShopAnnouncementSetExecutor.java
@@ -0,0 +1,33 @@
+package com.elem.retail.api.executor;
+
+import com.elem.retail.api.ElemRequest;
+import elem.retail.dto.shop.ShopAnnouncementSetRequest;
+
+import java.io.Serializable;
+
+/**
+ * 设置商户公告。
+ *
+ * @Author zhouw
+ * @Description 设置商户公告
+ * @Date 2021-09-09
+ */
+public class ShopAnnouncementSetExecutor extends ElemApiExecutor {
+
+    private ShopAnnouncementSetRequest request;
+
+    public ShopAnnouncementSetExecutor(String appid, String secret, ShopAnnouncementSetRequest request) {
+        super(appid, secret);
+        this.request = request;
+    }
+
+    @Override
+    ElemRequest getRequest() {
+        return request;
+    }
+
+    @Override
+    Class<? extends Serializable> getResponseDataClass() {
+        return String.class;
+    }
+}
---
+++ src/main/java/elem/retail/dto/shop/ShopAnnouncementSetRequest.java
@@ -0,0 +1,42 @@
+package elem.retail.dto.shop;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.annotation.JSONField;
+import com.elem.retail.api.BaseElemRequest;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @Author zhouw
+ * @Description
+ * @Date 2021-09-09
+ */
+@Getter
+@Setter
+public class ShopAnnouncementSetRequest extends BaseElemRequest {
+
+    private static final long serialVersionUID = 2268125330726465652L;
+
+    @JSONField(name = "baidu_shop_id")
+    private String baiduShopId;
+    @JSONField(name = "shop_id")
+    private String shopId;
+    private String content;
+    @JSONField(name = "descritption")
+    private String description;
+
+    @Override
+    public String getCmd() {
+        return "shop.announcement.set";
+    }
+
+    @Override
+    public String getBody() {
+        return JSON.toJSONString(this);
+    }
+
+    @Override
+    public String getKeyword() {
+        return baiduShopId;
+    }
+}
