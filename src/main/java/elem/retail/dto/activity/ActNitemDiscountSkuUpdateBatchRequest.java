652553d7fbb397f7bf83a7da6a38d9ecae688957
周伟 <vszhou_wei2007@126.com>
2021-10-08 11:53:25 +0800

    【第N件特价真折扣】商品第N件特价真折扣批量修改商品明细

---
+++ src/main/java/elem/retail/dto/activity/ActNitemDiscountSkuUpdateBatchRequest.java
@@ -0,0 +1,63 @@
+package elem.retail.dto.activity;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.annotation.JSONField;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.elem.retail.api.BaseElemRequest;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+/**
+ * @author zhouw
+ * @description
+ * @date 2021-10-08
+ */
+@Getter
+@Setter
+public class ActNitemDiscountSkuUpdateBatchRequest extends BaseElemRequest {
+
+    private static final long serialVersionUID = -8716848448189108941L;
+
+    @JSONField(name = "activity_id")
+    private Long activityId;
+
+    @JSONField(name = "act_sku_info")
+    private List<ActSkuInfo> actSkuInfos;
+
+    @Getter
+    @Setter
+    public static class ActSkuInfo {
+        @JSONField(name = "baidu_shop_id")
+        private String baiduShopId;
+        @JSONField(name = "shop_id")
+        private String shopId;
+        @JSONField(name = "sku_id")
+        private String skuId;
+        @JSONField(name = "custom_sku_id")
+        private String customSkuId;
+        @JSONField(name = "day_stock")
+        private Integer dayStock;
+        private Integer stock;
+        @JSONField(name = "store_id")
+        private String storeId;
+        @JSONField(name = "item_id")
+        private String itemId;
+    }
+
+    @Override
+    public String getCmd() {
+        return "act.nitemdiscount.sku.update.batch";
+    }
+
+    @Override
+    public String getBody() {
+        return JSON.toJSONString(this, SerializerFeature.DisableCircularReferenceDetect);
+    }
+
+    @Override
+    public String getKeyword() {
+        return null;
+    }
+}
